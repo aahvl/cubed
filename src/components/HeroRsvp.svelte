13d06efa0dc962e05ef54992661c10c8c779a16c
Chika Ugorji <chikaugorji@outlook.com>
2026-08-14 18:23:41 -0700

    made it mobile friendly
    yippiieeee its friendly on mobile screens

--- src/components/HeroRsvp.svelte
+++ src/components/HeroRsvp.svelte
@@ -2,15 +2,15 @@
 
 </script>
 
-<div class="h-auto w-full flex flex-row justify-center gap-4 pointer-events-auto">
+<div class="h-auto w-full flex flex-col md:flex-row items-center md:justify-center gap-3 md:gap-4 pointer-events-auto">
     <input 
     type="text" 
     name="email" 
     placeholder="Enter your email..."
-    class="block bg-white w-[28vw] outline-4 outline-black/85 px-5 py-4 text-2xl text-black/80 hover:outline-black/75 hover:mr-2 transitions-all duration-200 ease-in-out"
+    class="block bg-white w-full md:w-[28vw] outline-4 outline-black/85 px-4 md:px-5 py-3 md:py-4 text-lg md:text-2xl text-black/80 text-center md:text-left hover:outline-black/75 md:hover:mr-2 transitions-all duration-200 ease-in-out"
     >
     <button
-    class="bg-white outline-4 outline-black/85 px-[2vw] py-[2vh] text-2xl hover:bg-grey-300 hover:outline-black/75 hover:scale-110 hover:ml-3 active:bg-white active:outline-black active:scale-85 active:ml-4 transitions-all duration-200 ease-in-out"
+    class="bg-white outline-4 outline-black/85 px-[6vw] md:px-[2vw] py-[2vh] text-lg md:text-2xl hover:bg-grey-300 hover:outline-black/75 hover:scale-110 md:hover:ml-3 active:bg-white active:outline-black active:scale-85 active:ml-4 transitions-all duration-200 ease-in-out"
     >
         Get started →
     </button>
