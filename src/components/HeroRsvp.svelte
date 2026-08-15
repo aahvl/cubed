6e0563660df9f1e2d838f187885af7a9bd011171
Chika Ugorji <chikaugorji@outlook.com>
2026-08-15 14:30:01 -0700

    Optimized HexagonBG - optimized site for slower devices - Added Darkmode

--- src/components/HeroRsvp.svelte
+++ src/components/HeroRsvp.svelte
@@ -10,7 +10,7 @@
     class="block bg-white w-full md:w-[28vw] outline-4 outline-black/85 px-4 md:px-5 py-3 md:py-4 text-lg md:text-2xl text-black/80 text-center md:text-left hover:outline-black/75 md:hover:mr-2 transitions-all duration-200 ease-in-out"
     >
     <button
-    class="bg-white outline-4 outline-black/85 px-[6vw] md:px-[2vw] py-[2vh] text-lg md:text-2xl hover:bg-grey-300 hover:outline-black/75 hover:scale-110 md:hover:ml-3 active:bg-white active:outline-black active:scale-85 active:ml-4 transitions-all duration-200 ease-in-out"
+    class="bg-white outline-4 outline-black/85 px-[6vw] md:px-[2vw] py-[2vh] text-lg md:text-2xl text-black hover:bg-grey-300 hover:outline-black/75 hover:scale-110 md:hover:ml-3 active:bg-white active:outline-black active:scale-85 active:ml-4 transitions-all duration-200 ease-in-out"
     >
         Get started →
     </button>
